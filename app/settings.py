fac17ea1e2f18d43623af3d4a194c7c87793552c
Bakhtiyar Nikolayev <storyteller0205@gmail.com>
2022-11-29 05:15:30 +0600

    1

--- app/settings.py
+++ app/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fh4j2up%)te%cklsf4&qkizq51qu%@+=sc1gb6ee_b0uopd*r@'
+SECRET_KEY = 'fdsfdsjkl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
